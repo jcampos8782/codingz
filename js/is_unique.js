3fabf1415aafca79ceb20b1d92afdb9204b7b922
Jason Campos <jcampos8782@gmail.com>
2020-01-13 21:12:33 -0800

    One-liner for unique string check

--- js/is_unique.js
+++ js/is_unique.js
@@ -2,31 +2,21 @@
  Determines if a string has all unique characters
 */
 function isUnique(str) {
-  // Blow up the string and sort it. If two adjacent characters
-  // are the same, the string does not have unique characters
-  let sortedCharArray = str.split('').sort();
-  for(i = 1; i < sortedCharArray.length; i++) {
-    if(sortedCharArray[i - 1] === sortedCharArray[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function isUnique2(str) {
-    let chars = {};
-    str.split('').forEach(c => chars[c] = true);
-    return Object.keys(chars).length === str.length;
+  // Blow up the string and reduce the characters into a hash of keys.
+  // If count of keys is same length as original string, then all
+  // characters were unique.
+  return Object.keys(str.split('').reduce((acc,b) => {
+    acc[b] = 1;
+    return acc;
+  }, {})).length === str.length;
 }
 
 let tests = [
   ["abccdΩa", false],
-  ["abcdefg", true]
+  ["abcdefg", true],
+  ["the quick-brown_fax", true],
+  ["this quick brown fox", false]
 ];
 
 console.log("isUnique()");
 tests.map(a => console.log(`${isUnique(a[0]) === a[1] ? "PASS" : "FAIL!"}`));
-console.log("\n");
-
-console.log("isUnique2()");
-tests.map(a => console.log(`${isUnique2(a[0]) === a[1] ? "PASS" : "FAIL!"}`));
